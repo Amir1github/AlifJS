90e74e611aea2af38c171a8446968af0b2b0ee02
Amir <121044080+Amir1github@users.noreply.github.com>
2024-09-16 19:23:29 +0500

    Update ezpicit-conversion-boolean.js

--- ezpicit-conversion-boolean.js
+++ ezpicit-conversion-boolean.js
@@ -1,33 +1,10 @@
 function check(a){
-switch(a){
-  case false:
-    return Boolean(false);         // false
-    break;
-  case undefined:
-    return Boolean(undefined);     // false
-    break;
-  case null:
-    Boolean(null);          // false
-    break;
-  case '':
-    return Boolean('');            // false
-    break;
-  case NaN:
-    return Boolean(NaN);           // false
-    break;
-  case 0:
-    return Boolean(0);             // false
-    break;
-  case -0:
-    return Boolean(-0);            // false
-    break;
-  case 0n:
-    return Boolean(0n);            // false
-    break;
-  default:
-    return true; 
+  if(a ===false || a===0n || a===-0 || a===0|| a===''||a===null||a===undefined || isNaN(a)){
+    return Boolean(a);   //false
+  }else{
+    return Boolean(a);   //true
     //Выводится true в случае непустой строки, любого числа кроме 0; -0, объекта и булевого значения true
-}
+  }
 }
 let a = 0;
 console.log(`Булевое значение переменной - ${check(a)}`);
