e8e780c691cda78121098845a74e9f716e6e7c82
Amir <121044080+Amir1github@users.noreply.github.com>
2024-09-16 19:41:21 +0500

    Create explicit-conversion-number.js

---
+++ explicit-conversion-number.js
@@ -0,0 +1,12 @@
+function check(a){
+  if(a ===undefined || a!=''|| isNumber(a)){
+    return Number(a);   //NaN
+  }else if(a===null || a===false){
+    return Number(a);   //0
+  }else{
+    return Number(a); 
+    //string содежащий число преобразуется в Number, объекты, true в 1
+  }
+}
+let a = "ab";
+console.log(`Булевое значение переменной - ${check(a)}`);
